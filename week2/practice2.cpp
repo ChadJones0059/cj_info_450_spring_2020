60246eb1c36be175ab6d391df8cefe3ed396971f
Chad Jones <48305153+ChadJones0059@users.noreply.github.com>
2020-01-28 21:21:02 -0500

    commit

--- week2/practice2.cpp
+++ week2/practice2.cpp
@@ -12,14 +12,19 @@ int main()
         cout << "Please enter a positive number or press 0 to exit. ";
         cin >> userInput;
         
-        for(i = 2; i <= userInput; i++)     //initialize i set to 2. Our condition 2 <= userInput, once ran, add +1 to i 
+        for(i = 2; i <= userInput; ++i)     //initialize i set to 2. Our condition 2 <= userInput, once ran, add +1 to i 
         {
             if(userInput % i == 0)     //if userInput / 2,3,4... has 0 remainder, then it's NOT prime
             {
                 prime = false;      //has 0 remainder, prime is not true
-                i++;
                 break;              //break from loop
             }
+            else
+            {
+                prime = true;
+                break;
+            }
+            
         }
         if(prime == true)           //check bool statement if true
         {
