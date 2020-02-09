57eff2ec605817dd71a48ff1aee0c9e533e2b676
Chad Jones <48305153+ChadJones0059@users.noreply.github.com>
2020-02-08 23:38:59 -0500

    commit

--- week4/program2.cpp
+++ week4/program2.cpp
@@ -5,10 +5,18 @@ using namespace std;
 int main()
 {
   char student[30][100];
-  for(int i = 0; i < 31; i++)
+  for(int i = 0; i < 30; i++)
   {
     cout << "Please enter student for index " << i << ": ";
     cin.getline(student[i],100);
+    if(student[i] == 0)
+    {
+      break;
+    }
+  }
+  for(int i =0; i < 30; i++)
+  {
+    cout << "students[" << i << "] = " << student[i] << endl;
   }
   return 0;
 }
