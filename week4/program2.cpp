d4bf63fbdf5aafd37964f747495851c5d9fe4cb0
Chad Jones <48305153+ChadJones0059@users.noreply.github.com>
2020-02-10 15:33:59 -0500

    commit

--- week4/program2.cpp
+++ week4/program2.cpp
@@ -3,18 +3,22 @@ using namespace std;
 
 int main()
 {
-  char student[30][100];
-  for(int i = 0; i < 30; i++)
+  char student[30][100];    // two dimensional array
+  for(int i = 0; i < 30; i++)   //start for loop for user to enter 2 strings
   {
-    cout << "Please enter student for index " << i << ": ";
-    cin.getline(student[i],100);
-    if(student[i] == 0)
+    cout << "Please enter student for index " << i << ": ";     // print output
+    cin.getline(student[i],100);                                // store variable
+    if(student[i][i] == 0)                         // if user presses enter, break from loop
+    {
+      break;  
+    }
+  }
+  for(int i =0; i < 30; i++)                  
+  {
+    if(student[i][i] == 0)
     {
       break;
     }
-  }
-  for(int i =0; i < 30; i++)
-  {
     cout << "students[" << i << "] = " << student[i] << endl;
   }
   return 0;
